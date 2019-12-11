515f6fe4d1906578c1111737a7e333005a9acdf6
gomesmor <rui.monteiro@roche.com>
2019-12-11 12:32:58 +0100

    d4 complete solution and validated answer

--- AoCPython/main2019.py
+++ AoCPython/main2019.py
@@ -1,5 +1,12 @@
-from AoC2019 import d1
+from AoC2019 import d4
 
-a = d1.d1()
-a.get_total_fuel_2()
-print(a.total_fuel)
+print("exercice 4.1")
+d4_input = "353096-843212"
+a41 = d4.d4(d4_input)
+a41.calc_num_passwords_1()
+print("the answer is {}".format(a41.number_pwds))
+
+print("exercice 4.2")
+a42 = d4.d4(d4_input)
+a42.calc_num_passwords()
+print("the answer is {}".format(a42.number_pwds))
