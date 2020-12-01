4e1313494706ba982e93149afab574c89e60ee84
“Orlando <orlando.pereira@roche.com>
2020-12-01 15:28:11 +0100

    AOC 2020 Day 1 complete source. Xcode struture refinement

---
+++ AoC Swift/AdventOfCode/2020/ExpenseReport.swift
@@ -0,0 +1,24 @@
+//
+//  ExpenseReportCalculator.swift
+//  AdventOfCode
+//
+//  Created by Pereira Orlando on 01.12.20.
+//  Copyright © 2020 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
+
+class ExpenseReport {
+    
+    private var answer: Int
+    
+    init() {
+        self.answer = 0
+        calculator()
+    }
+    
+    private func calculator() {
+        print(self.answer)
+    }
+    
+}
---
+++ AoC Swift/AdventOfCode/Utils/ExerciseInputs.swift
@@ -0,0 +1,9 @@
+//
+//  ExerciseInputs.swift
+//  AdventOfCode
+//
+//  Created by Pereira Orlando on 01.12.20.
+//  Copyright © 2020 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
