5037f6f0dc0f1dad5abf9bcba56ea4e5fd6ecd7c
Orlando Pereira <orepereira@gmail.com>
2020-12-03 16:16:34 +0100

    Day 3 complete with refactoring. Added rules to .gitignore

---
+++ AoC Swift/AdventOfCode/2020/TobogganTrajectory.swift
@@ -0,0 +1,9 @@
+//
+//  TobogganTrajectory.swift
+//  AdventOfCode
+//
+//  Created by Pereira Orlando on 03.12.20.
+//  Copyright © 2020 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
