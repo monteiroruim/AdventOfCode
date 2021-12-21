fab523f2e40d439775d14055598f03dbaab30aed
Orlando Pereira <orepereira@gmail.com>
2021-12-21 13:37:21 +0100

    2021 Adding remaining days structures

---
+++ AoC Swift/AdventOfCode/2021/Inputs/In2021D25.swift
@@ -0,0 +1,31 @@
+//
+//  In2021D25.swift
+//  AdventOfCode
+//
+//  Created by Pereira Orlando on 21.12.21.
+//  Copyright © 2021 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
+
+class In2021D25 {
+    
+    private var inputDebug = """
+
+"""
+    private var input = """
+
+"""
+    
+    public init() {
+    }
+    
+    public func getInput() -> String {
+        return input
+    }
+    
+    public func getInputDebug() -> String {
+        return inputDebug
+    }
+    
+}
---
+++ AoC Swift/AdventOfCode/2021/TwentyOneDay21.swift
@@ -0,0 +1,34 @@
+//
+//  TwentyOneDay18.swift
+//  AdventOfCode
+//
+//  Created by Pereira Orlando on 21.12.21.
+//  Copyright © 2021 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
+
+class TwentyOneDay21 {
+    
+    //private var input: [String]
+    
+    public init() {
+        print("puzzle answer (part 1): \(PartOne())")
+        print("puzzle answer (part 2): \(PartTwo())")
+        Tests()
+    }
+    
+    private func PartOne() -> Int {
+        return 0
+    }
+    
+    private func PartTwo() -> Int {
+        return 0
+    }
+
+    private func Tests() {
+        assert(PartOne() == 0, "PartOne KO")
+        assert(PartTwo() == 0, "PartTwo KO")
+    }
+    
+}
