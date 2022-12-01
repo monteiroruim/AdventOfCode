0ec6954d091854ba60093bb08388e0e0a2520e50
Orlando Pereira <orepereira@gmail.com>
2022-12-01 16:25:45 +0100

    2022 Day 2 structure

---
+++ AoC Swift/AdventOfCode/2022/In2022D02.swift
@@ -0,0 +1,31 @@
+//
+//  In2022D02.swift
+//  AdventOfCode
+//
+//  Created by Orlando Pereira on 01.12.22.
+//  Copyright © 2022 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
+
+class In2022D02 {
+    
+    private var inputDebug = """
+
+"""
+    private var input = """
+
+"""
+    
+    public init() {
+    }
+    
+    public func getInput() -> String {
+        return input
+    }
+    
+    public func getInputDebug() -> String {
+        return inputDebug
+    }
+    
+}
---
+++ AoC Swift/AdventOfCode/2022/TwentyTwoDay02.swift
@@ -0,0 +1,34 @@
+//
+//  TwentyTwoDay02.swift
+//  AdventOfCode
+//
+//  Created by Orlando Pereira on 01.12.22.
+//  Copyright © 2022 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
+
+class TwentyTwoDay02 {
+    
+    //private var input: [String]
+    
+    public init() {
+        print("puzzle answer (part 1): \(PartOne())")
+        print("puzzle answer (part 2): \(PartTwo())")
+        //Tests()
+    }
+    
+    private func PartOne() -> Int {
+        return 0
+    }
+    
+    private func PartTwo() -> Int {
+        return 0
+    }
+
+    private func Tests() {
+        assert(PartOne() == 0, "PartOne KO")
+        assert(PartTwo() == 0, "PartTwo KO")
+    }
+    
+}
