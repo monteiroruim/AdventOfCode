12dcfc4754432cc5ff5aa658c01ee928fecbeea6
Orlando Pereira <orepereira@gmail.com>
2022-12-01 08:37:06 +0100

    2022 Day 1 complete

--- AoC Swift/AdventOfCode/2022/TwentyTwoDay01.swift
+++ AoC Swift/AdventOfCode/2022/TwentyTwoDay01.swift
@@ -7,28 +7,56 @@
 //
 
 import Foundation
+import Algorithms
 
 class TwentyTwoDay01 {
     
-    //private var input: [String]
+    private var input: [String]
+    private var totalCalories: [Int]
     
     public init() {
+        self.input = In2022D01().getInput().components(separatedBy: "\n\n")
+        totalCalories = self.input.map { line in
+            line
+                .components(separatedBy: CharacterSet.newlines)
+                .map{ Int($0)! }
+                .reduce(0, +)
+        }
+        
         print("puzzle answer (part 1): \(PartOne())")
         print("puzzle answer (part 2): \(PartTwo())")
         //Tests()
     }
     
     private func PartOne() -> Int {
-        return 0
+        
+        // Idiomatic swift
+        return totalCalories.max()!
+        
+        // non idiomatic
+//        var max = 0
+//        for i in totalCalories {
+//            if i > max { max = i }
+//        }
+//        return(max)
     }
     
     private func PartTwo() -> Int {
-        return 0
+        
+        // idiomatic swift
+        return totalCalories.sorted(by: >).prefix(3).reduce(0, +)
+        
+//        var max = 0
+//        let calSorted = totalCalories.sorted(by: >)
+//        for i in 0...2 {
+//            max += calSorted[i]
+//        }
+//        return(max)
     }
 
     private func Tests() {
-        assert(PartOne() == 0, "PartOne KO")
-        assert(PartTwo() == 0, "PartTwo KO")
+        assert(PartOne() == 69795, "PartOne KO")
+        assert(PartTwo() == 208437, "PartTwo KO")
     }
     
 }
