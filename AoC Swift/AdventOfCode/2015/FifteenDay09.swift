fc1ab9b9a052085fb6f9b9ea614f7f3bf1146503
Orlando Pereira <orepereira@gmail.com>
2022-12-04 14:17:34 +0100

    2015 day 09 complete swift idiomatic

--- AoC Swift/AdventOfCode/2015/FifteenDay09.swift
+++ AoC Swift/AdventOfCode/2015/FifteenDay09.swift
@@ -11,21 +11,24 @@ import Algorithms
 
 class FifteenDay09 {
     
-    private var input = In2015D09().inputDebug.components(separatedBy: CharacterSet.newlines)
+    private var input = In2015D09().input.components(separatedBy: CharacterSet.newlines)
     private var uniqueCities: Set<String>
+    private var costs: [String: Int]
     
     public init() {
         
         self.uniqueCities = Set<String>()
+        self.costs = [String: Int]()
         
-        print("puzzle answer (part 1): \(PartOne())")
-        print("puzzle answer (part 2): \(PartTwo())")
-        //Tests()
+        print("puzzle answer (part 1): \(PartOne().0)")
+        print("puzzle answer (part 2): \(PartOne().1)")
+        Tests()
     }
     
-    private func PartOne() -> Int {
+    private func PartOne() -> (Int, Int) {
         
         var min = Int.max
+        var max = 0
         
         for i in input {
             let line = i.components(separatedBy: CharacterSet.whitespaces)
@@ -33,26 +36,32 @@ class FifteenDay09 {
             let destination = line[1]
             uniqueCities.insert(origin)
             uniqueCities.insert(destination)
-            //let cost = line[4]
+            let cost = Int(line[2])!
+            self.costs[origin + " " + destination] = cost
         }
-        //print("Unique:", uniqueCities)
         
         for perm in uniqueCities.uniquePermutations() {
-            print(perm)
-        }
+            var cost = 0
+            for i in 1..<perm.count {
+                let start = perm[i-1]
+                let end = perm[i]
                 
-        return min
-        // 207
-        // 764
-    }
-    
-    private func PartTwo() -> Int {
-        0
+                if (self.costs[start + " " + end] ?? 0) != 0 {
+                    cost += self.costs[start + " " + end] ?? 0
+                } else {
+                    cost += self.costs[end + " " + start] ?? 0
+                }
+            }
+            if cost < min { min = cost }
+            if cost > max { max = cost}
+        }
+        
+        return (min, max)
     }
     
     private func Tests() {
-        assert(PartOne() == 0, "Failed at PartOne")
-        assert(PartTwo() == 0, "Failed at PartTwo")
+        assert(PartOne().0 == 207, "Failed at PartOne")
+        assert(PartOne().1 == 804, "Failed at PartTwo")
     }
     
 }
