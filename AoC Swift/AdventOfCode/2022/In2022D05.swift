b947a8b4182866d50c8384ba0863416628ae4758
Orlando Pereira <orepereira@gmail.com>
2022-12-05 10:25:50 +0100

    2022 Day 5 structure

---
+++ AoC Swift/AdventOfCode/2022/In2022D05.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  AdventOfCode
+//
+//  Created by Orlando Pereira on 05.12.22.
+//  Copyright © 2022 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
+
+class In2022D05 {
+    
+    private var inputDebug = """
+
+"""
+    private var input = """
+
+"""
+    
+    public init() {
+    }
+    
+    public func getInput() -> String {
+        return input
+    }
+    
+    public func getInputDebug() -> String {
+        return inputDebug
+    }
+    
+}
---
+++ AoC Swift/AdventOfCode/2022/TwentyTwoDay05.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  AdventOfCode
+//
+//  Created by Orlando Pereira on 05.12.22.
+//  Copyright © 2022 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
+
+class TwentyTwoDay05 {
+    
+    //private var input: [String]
+    
+    public init() {
+        print("puzzle answer (part 1): \(PartOne())")
+        print("puzzle answer (part 2): \(PartTwo())")
+        //Tests()
+    }
+    
+    private func PartOne() -> Int {
+        return 0
+    }
+    
+    private func PartTwo() -> Int {
+        return 0
+    }
+
+    private func Tests() {
+        assert(PartOne() == 0, "Part 1 KO")
+        assert(PartTwo() == 0, "Part 2 KO")
+    }
+    
+}
