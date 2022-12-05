23d0a54bcd61c83cf464c05ebbcdec68e31023ac
Orlando Pereira <orepereira@gmail.com>
2022-12-05 11:57:02 +0100

    2022 Day 05 Complete

--- AoC Swift/AdventOfCode/2022/TwentyTwoDay05.swift
+++ AoC Swift/AdventOfCode/2022/TwentyTwoDay05.swift
@@ -7,28 +7,84 @@
 //
 
 import Foundation
+import Collections
+import Algorithms
+import RegexBuilder
 
 class TwentyTwoDay05 {
     
-    //private var input: [String]
+    private var input: [String]
     
     public init() {
+        
+        self.input = In2022D05().getInput().components(separatedBy: "\n\n")
+        
         print("puzzle answer (part 1): \(PartOne())")
         print("puzzle answer (part 2): \(PartTwo())")
-        //Tests()
+        Tests()
     }
     
-    private func PartOne() -> Int {
-        return 0
+    private func PartOne() -> String {
+        var result = ""
+        
+        var stacks = parseStacks(from: self.input.first!)
+        let instructions = self.input.last!.components(separatedBy: CharacterSet.newlines)
+        
+        for i in instructions {
+            let op = i.components(separatedBy: " ")
+            let amount = Int(op[1])!
+            let from = Int(op[3])!
+            let to = Int(op[5])!
+            for _ in 0..<amount {
+                let value = stacks[from]!.popLast()!
+                stacks[to]?.append(value)
+            }
+        }
+
+        for i in stacks.keys.sorted() {
+            result += stacks[i]!.last!.description
+        }
+        return result
     }
     
-    private func PartTwo() -> Int {
-        return 0
+    private func PartTwo() -> String {
+        var result = ""
+        
+        var stacks = parseStacks(from: self.input.first!)
+        let instructions = self.input.last!.components(separatedBy: CharacterSet.newlines)
+        
+        for i in instructions {
+            let op = i.components(separatedBy: " ")
+            let amount = Int(op[1])!
+            let from = Int(op[3])!
+            let to = Int(op[5])!
+        
+            let values = stacks[from]!.suffix(amount)
+            stacks[from]!.removeLast(amount)
+            stacks[to]!.append(contentsOf: values)
+        }
+
+        for i in stacks.keys.sorted() {
+            result += stacks[i]!.last!.description
+        }
+        return result
+    }
+    
+    func parseStacks(from input: String) -> [Int: [Character]] {
+        input
+            .split(separator: "\n")
+            .reduce(into: [Int: [Character]]()) { stacks, line in
+                for (i, c) in line.enumerated() {
+                    guard c.isLetter else { continue }
+                    let stackNumber = (i / 4) + 1
+                    stacks[stackNumber, default: []].insert(c, at: 0)
+                }
+            }
     }
 
     private func Tests() {
-        assert(PartOne() == 0, "Part 1 KO")
-        assert(PartTwo() == 0, "Part 2 KO")
+        assert(PartOne() == "ZRLJGSCTR", "Part 1 KO")
+        assert(PartTwo() == "PRTTGRFPB", "Part 2 KO")
     }
     
 }
--- AoC Swift/AdventOfCode/main.swift
+++ AoC Swift/AdventOfCode/main.swift
@@ -78,5 +78,6 @@ let startTime = CFAbsoluteTimeGetCurrent()
 //var twentyTwoDay02 = TwentyTwoDay02()
 //var twentyTwoDay03 = TwentyTwoDay03()
 //var twentyTwoDay04 = TwentyTwoDay04()
+var twentyTwoDay05 = TwentyTwoDay05()
 
 print("\nExec time: ", NSString(format: "%.5f", CFAbsoluteTimeGetCurrent() - startTime), "s")
