261fae15bf2538bce927cca77a3a8045dbce15c4
Orlando Pereira <orepereira@gmail.com>
2022-12-08 15:44:37 +0100

    2022 day 09 structure

---
+++ AoC Swift/AdventOfCode/2022/In2022D09.swift
@@ -0,0 +1,31 @@
+//
+//  In2022D09.swift
+//  AdventOfCode
+//
+//  Created by Orlando Pereira on 08.12.22.
+//  Copyright © 2022 Pereira Orlando. All rights reserved.
+//
+
+import Foundation
+
+class In2022D09 {
+    
+    private var inputDebug = """
+
+"""
+    private var input = """
+
+"""
+    
+    public init() {
+    }
+    
+    public func getInput() -> String {
+        return input
+    }
+    
+    public func getInputDebug() -> String {
+        return inputDebug
+    }
+    
+}
