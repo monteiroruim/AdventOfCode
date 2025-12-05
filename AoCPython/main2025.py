d6d81f21b3ffd3c867beec47a6c424afef652fe4
Rui Monteiro | Fibersail <rui.monteiro@fibersail.com>
2025-12-05 09:35:47 +0000

    feat: day 2 solution

--- AoCPython/main2025.py
+++ AoCPython/main2025.py
@@ -1,9 +1,28 @@
 import os
 import time
 from utils import utils
-from AoC2025 import d1
+from AoC2025 import d1, d2
 
-# day 21
+# day 2
+print("Day 2: Gift Shop")
+start_time = time.time()
+print("- testing")
+tst = d2.test().run()
+
+start_time = time.time()
+
+print("- calculation")
+inputHandler = utils.fileHandler()
+input_set = inputHandler.split_line_to_list(os.getcwd() 
+    + "/AoCPython/AoC2025/input/d2.txt")
+solution = d2.d2(input_set)
+solution.resolve()
+print("part1 :", solution.get_res_pt1()) # 13919717792
+print("part2 :", solution.get_res_pt2()) # 14582313461
+print("--- %s seconds ---" % (time.time() - start_time))
+exit()
+
+# day 1
 print("Day 1: Secret Entrance")
 start_time = time.time()
 print("- testing")
