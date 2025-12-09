9480df3fdb835635f39a1b73f36a84861b717846
Rui Monteiro | Fibersail <rui.monteiro@fibersail.com>
2025-12-09 19:16:21 +0000

    chore: day 6 part 2

--- AoCPython/AoC2025/d6.py
+++ AoCPython/AoC2025/d6.py
@@ -30,6 +30,8 @@ class d6:
         self.operators = init_data[-1].split()
         print(self.data)
         print(self.operators)
+        self.cephalopod_data = [v for v in init_data[:-1]]
+        print(self.cephalopod_data)
 
     
     def solve_part_1(self)-> int:
@@ -39,16 +41,49 @@ class d6:
             for v in self.data:
                 operands.append(int(v[i]))
             if self.operators[i] == self.operator_multiply:
-                print("multiply {self.operators[i]} in list {operands}")
+                #print(f"multiply {self.operators[i]} in list {operands}")
                 res += prod(operands)
             else:
-                print("add {self.operators[i]} in list {operands}")
+                #print(f"add {self.operators[i]} in list {operands}")
                 res += sum(operands)
 
         return res
 
     def solve_part_2(self)-> int:
+        
+        # math is written right-to-left in columns. Each number is given in its own column, 
+        # with the most significant digit at the top and the least significant digit at the bottom
         res = 0
+        new_ops = list()
+
+        operands = list()
+        for i in range(len(self.cephalopod_data[0])):
+            numbers = list()
+            n = ""
+            for j in range(len(self.cephalopod_data)):
+                n += self.cephalopod_data[j][i]
+
+                numbers.append(n)
+            if n.isspace():
+                #print("break line", n)
+                new_ops.append(operands)
+                operands = list()
+                continue
+            else:
+                #print("cephalopod ", numbers[-1])
+                operands.append(int(numbers[-1]))
+        
+        new_ops.append(operands)
+        #print(new_ops)
+        for i in range(len(self.operators)):
+            operands = list()
+            if self.operators[i] == self.operator_multiply:
+                #print(f"multiply {self.operators[i]} in list {operands}")
+                res += prod(new_ops[i])
+            else:
+                #print(f"add {self.operators[i]} in list {operands}")
+                res += sum(new_ops[i])
+
         return res
 
     def resolve(self) -> None:
@@ -61,14 +96,14 @@ class d6:
 import unittest
 class test(unittest.TestCase):
     def run(self):
-        test_set = ["123 328  51 64", 
-                    "45 64  387 23",
-                    "6 98  215 314",
-                    "*   +   *   + "]
+        test_set = ["123 328  51 64 ", 
+                    " 45 64  387 23 ",
+                    "  6 98  215 314",
+                    "*   +   *   +  "]
 
         init = d6(test_set)
         init.resolve()
         self.assertEqual(init.test_pt1(), 4277556) # 
-        self.assertEqual(init.test_pt2(), 0)
+        self.assertEqual(init.test_pt2(), 3263827)
     
 
